4c108b2ed23a44bf3a70d5b00ce5ba34a6b613d1
Mert Akçakaya <mert@ad.biltera.com.tr>
2016-04-15 17:00:14 +0300

    SignalR Game Hub creation
    Created GameHub class inherited from SignalR base class called Hub to
    enable communication with the client side SignalR and push the calls to
    Actor system.

--- Game/Game.ActorModel/ExternalSystems/IGameEventsPusher.cs
+++ Game/Game.ActorModel/ExternalSystems/IGameEventsPusher.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Game.ActorModel.ExternalSystems
+﻿namespace Game.ActorModel.ExternalSystems
 {
     public interface IGameEventsPusher
     {
---
+++ Game/Game.Web/Models/GameHub.cs
@@ -0,0 +1,18 @@
+﻿using Game.ActorModel.Messages;
+using Microsoft.AspNet.SignalR;
+
+namespace Game.Web.Models
+{
+    public sealed class GameHub : Hub
+    {
+        public void JoinGame(string playerName)
+        {
+            GameActorSystem.ActorReferences.SignalRBridge.Tell(new JoinGameMessage(playerName));
+        }
+
+        public void Attack(string playerName)
+        {
+            GameActorSystem.ActorReferences.SignalRBridge.Tell(new AttackPlayerMessage(playerName));
+        }
+    }
+}
