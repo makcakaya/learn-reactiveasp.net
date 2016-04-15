e73e11560f3159a640f943b0f207cde4b0937d75
Mert Akçakaya <mert@ad.biltera.com.tr>
2016-04-15 16:50:07 +0300

    SignalR addition
    SignalR package is added and wire-up code is committed.
    IGameEventsPusher interface is defined.

---
+++ Game/Game.ActorModel/ExternalSystems/IGameEventsPusher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.ActorModel.ExternalSystems
+{
+    public interface IGameEventsPusher
+    {
+        void PlayerJoined(string playerName, int playerHealth);
+        void UpdatePlayerHealth(string playerName, int playerHealth);
+    }
+}
